049db18dc46fce6c90b96b23c8c893c1f78c38af
AJ Hsu <ajhsu@kkbox.com>
2015-09-22 13:52:08 +0800

    Update testing

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,54 @@
-let before = `
-  var test1 = function b(a,b,c){};
-  var test2 = [function c(a,b,c){}];
-  var test3 = (function d(c,d,e){return 'hello';})();
-  var test4 = {
-    test: function e(d,e,f){}
-  };
-  var test4 = {
-    test: !function e(d,e,f){}
-  };
-`;
-console.log(before);
+var expect = require('chai').expect;
+var snfe = require('../index.js');
 
-let after = before.replace(/([\=|\[|\,|\(|\:|\!])[\s]*function\s+([\w]+)(\([\w|\,|\s]*?\2[\w|\,|\s]*?\))/g, '$1function$3');
-console.log(after);
+describe('Basics', function() {
+
+    it('should strip function name', function() {
+
+        var testCases = [{
+            raw: 'var test1 = function b(a,b,c){};',
+            expected: 'var test1 = function(a,b,c){};'
+        }, {
+            raw: 'var test1 = function anonymous(a,anonymous,c){};',
+            expected: 'var test1 = function(a,anonymous,c){};'
+        }, {
+            raw: 'var test2 = [function c(a,b,c){}];',
+            expected: 'var test2 = [function(a,b,c){}];'
+        }, {
+            raw: 'var test3 = (function d(c,d,e){return "hello";})();',
+            expected: 'var test3 = (function(c,d,e){return "hello";})();'
+        }, {
+            raw: 'var test4 = { test: function e(d,e,f){} };',
+            expected: 'var test4 = { test: function(d,e,f){} };'
+        }, {
+            raw: 'var test4 = { test: !function e(d,e,f){} };',
+            expected: 'var test4 = { test: !function(d,e,f){} };'
+        }];
+
+        for (var c = 0; c < testCases.length; c++) {
+            var raw = testCases[c].raw;
+            var expected = testCases[c].expected;
+            var result = snfe(raw);
+            expect(result).to.be.equal(expected);
+        }
+    });
+
+    it('should throw expecetion when input is empty', function(){
+        expect(function() {
+            snfe('');
+        }).to.throw('input was empty');
+    });
+
+    it('should throw expecetion when input is invalid', function(){
+        expect(function() {
+            snfe();
+        }).to.throw('input must be a string');
+    });
+
+});
+
+describe('Basics', function(){
+
+    it('Basic function');
+
+});
