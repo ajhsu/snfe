426355a8b89ce8b8dafe2a51d085f5a48b933ba6
AJ Hsu <ajhsu@kkbox.com>
2015-09-22 17:20:45 +0800

    Pull out search pattern and replacement as constants

--- src/index.es6.js
+++ src/index.es6.js
@@ -1,6 +1,8 @@
 'use strict';
 
 export default function stripNamedFunctionExpression(input) {
+export const SEARCH_PATTERN = /([\=|\[|\,|\(|\:|\!][\s]*)function\s+([\w]+)(\([\w|\,|\s]*?\2[\w|\,|\s]*?\))/g;
+export const REPLACEMENT = '$1function$3';
 
     if ('undefined' == typeof input) {
         throw `input must be a string`;
@@ -10,5 +12,5 @@ export default function stripNamedFunctionExpression(input) {
         return ``;
     }
 
-    return input.replace(/([\=|\[|\,|\(|\:|\!][\s]*)function\s+([\w]+)(\([\w|\,|\s]*?\2[\w|\,|\s]*?\))/g, '$1function$3');
+    return input.replace(SEARCH_PATTERN, REPLACEMENT);
 };
