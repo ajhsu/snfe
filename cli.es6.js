f88f054354eef245edbeda1c5e937d6d20c4276e
AJ Hsu <ajhsu@kkbox.com>
2015-09-22 13:51:14 +0800

    Refactor the help blocks

--- cli.es6.js
+++ cli.es6.js
@@ -4,14 +4,19 @@ import fs from 'fs';
 import snfe from './index';
 import { name, version } from './package.json';
 
-let input = process.argv[2];
+let printHelpMesage = () => {
+    console.log(`\nStrip Named Function Expression\n`);
+    console.log(`Usage:\n`);
+    console.log(`   snfe [input file] > [output file]`);
+    console.log(`\n${name}@${version} ${process.argv[1]}\n`);
+};
 
 if (process.argv.indexOf(`-h`) !== -1 || 
-    process.argv.indexOf(`--help`) !== -1) {
-    console.log(`snfe <input file> > <output file>`);
+    process.argv.indexOf(`--help`) !== -1 ||
+    process.argv.length === 2 ) {
+    printHelpMesage();
     process.exit(0);
-} else if (process.argv.indexOf(`-v`) !== -1 ||
-    process.argv.indexOf(`--version`) !== -1) {
+} else if (process.argv.indexOf(`-v`) !== -1 || process.argv.indexOf(`--version`) !== -1) {
     console.log(version);
     process.exit(0);
 }
