69f0e30a9339e0cad8d53f53475c3451ea03950c
AJ Hsu <ajhsu@kkbox.com>
2015-09-22 17:21:49 +0800

    Rename to `snfe` and remove from default function (needs to be call explicit)

--- src/index.es6.js
+++ src/index.es6.js
@@ -1,8 +1,8 @@
 'use strict';
 
-export default function stripNamedFunctionExpression(input) {
 export const SEARCH_PATTERN = /([\=|\[|\,|\(|\:|\!][\s]*)function\s+([\w]+)(\([\w|\,|\s]*?\2[\w|\,|\s]*?\))/g;
 export const REPLACEMENT = '$1function$3';
+export function snfe(input) {
 
     if ('undefined' == typeof input) {
         throw `input must be a string`;
