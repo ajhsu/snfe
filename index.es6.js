27ebac8b5180cb9732cd025cf07330790af14d3b
AJ Hsu <ajhsu@kkbox.com>
2015-09-22 13:46:29 +0800

    Add if-conditions for exception

--- index.es6.js
+++ index.es6.js
@@ -1,5 +1,14 @@
 'use strict';
 
-export default function stripNamedFunction(input) {
-    return input.replace(/([\=|\[|\,|\(|\:|\!])[\s]*function\s+([\w]+)(\([\w|\,|\s]*?\2[\w|\,|\s]*?\))/g, '$1function$3');
+export default function stripNamedFunctionExpression(input) {
+
+    if ('undefined' == typeof input) {
+        throw `input must be a string`;
+        return ``;
+    } else if (`` === input) {
+        throw `input was empty`;
+        return ``;
+    }
+
+    return input.replace(/([\=|\[|\,|\(|\:|\!][\s]*)function\s+([\w]+)(\([\w|\,|\s]*?\2[\w|\,|\s]*?\))/g, '$1function$3');
 };
